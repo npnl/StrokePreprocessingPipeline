a80ba2276041c1a95bef9d2117f8a0457f5190f4
Lex <50920802+AlexandreHutton@users.noreply.github.com>
2021-08-31 15:11:12 -0700

    added template masking for qc

--- nii_slice.py
+++ nii_slice.py
@@ -8,6 +8,7 @@ import sys
 def qc_slice(image_path: str,
              save_path: str,
              mask_path: str = None,
+             mask_template: str = None,
              nslices: int = 1):
     '''
     Extracts and plots slices in each plane from the specified 3D image. Optionally overlays a mask on the image. Slices
@@ -21,6 +22,8 @@ def qc_slice(image_path: str,
         Path where to save the resulting image.
     mask_path : str
         Optional. Mask to overlay. Default: None.
+    mask_template : str
+        Optional. Template mask to overlay. Default: None.
     nslices : int
         Optional. The number of slices to take.
 
@@ -34,6 +37,8 @@ def qc_slice(image_path: str,
         mask = nb.load(mask_path).get_fdata()
     if(not save_path.endswith('.png')):
         save_path += '.png'
+    if(mask_template is not None):
+        template = nb.load(mask_template).get_fdata()
 
     imshape = image.shape
     slice_ind = [np.linspace(0, imshape[i], nslices+2, dtype=int)[1:-1] for i in range(3)]
@@ -63,6 +68,9 @@ def qc_slice(image_path: str,
             if(mask_path is not None):
                 slice_im[:, start_ind: start_ind + mlt, 0] += \
                     ndimage.rotate(mask[slices[0], slices[1], slices[2]], 90)/np.max(mask) * 0.2*immax
+            if(mask_template is not None):
+                slice_im[:, start_ind: start_ind + mlt, 1] += \
+                    ndimage.rotate(template[slices[0], slices[1], slices[2]], 90)/np.max(template) * 0.2*immax
         full_img[prev_lim:prev_lim+slice_im.shape[0], :slice_im.shape[1], :] = slice_im/immax
         prev_lim += slice_im.shape[0]
 
@@ -76,9 +84,10 @@ def main(args):
     parser.add_argument('image', help='Path of the image to slice.')
     parser.add_argument('save_path', help='Path where to save QC image')
     parser.add_argument('--mask_path', default=None, help='Path to the mask file')
+    parser.add_argument('--mask_template', default=None, help='Path to the template mask file')
     parser.add_argument('--nslices', default=1, type=int, help='The number of slices to take in each direction.')
     pargs = parser.parse_args(args)
-    qc_slice(pargs.image, pargs.save_path, mask_path=pargs.mask_path, nslices=pargs.nslices)
+    qc_slice(pargs.image, pargs.save_path, mask_path=pargs.mask_path, nslices=pargs.nslices, mask_template=pargs.mask_template)
     return
 
 if __name__ == '__main__':
